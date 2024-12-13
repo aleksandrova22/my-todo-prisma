9f2d1a81e55a3f2328eff6bd47dc7a02df3956d7
aleksandrova22 <lialia1986@mail.ru>
2024-12-13 20:28:24 +0700

    1312+

--- components/Elem/List.tsx
+++ components/Elem/List.tsx
@@ -3,14 +3,14 @@ import { memo, Fragment } from 'react';
 import classes from './List.module.css';
 import type { AppProps } from "next/app";
 
-type Object = {
-  list, editedId, editForm
-};
+// type Object = {
+//   list: Array, editedId: number, editForm: object
+// };
 
-export const List = memo(function ({ list, editedId, editForm }: Object) {
+export const List = memo(function ({ list, editedId, editForm }) {
   console.debug('List render');
   return <fieldset className={classes.list}>
-    <legend>To do List</legend>
+    <legend>Список дел</legend>
     <ol>
       {list.map((item, id) =>
         String(item.id) === editedId
