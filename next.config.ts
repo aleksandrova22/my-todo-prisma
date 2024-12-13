8ed5eca898743c2125226d2dde5b9a2546f7bb45
aleksandrova22 <lialia1986@mail.ru>
2024-12-13 20:39:40 +0700

    1312++

--- next.config.ts
+++ next.config.ts
@@ -5,4 +5,11 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
+eslint: {
+  // Warning: This allows production builds to successfully complete even if
+  // your project has ESLint errors.
+  ignoreDuringBuilds: true
+}
+
+
 export default nextConfig;
